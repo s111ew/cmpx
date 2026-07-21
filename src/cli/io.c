918f988b939839601dff3452ffad2c65c8cb50e1
s111ew <167631312+s111ew@users.noreply.github.com>
2026-07-21 09:31:47 +0100

    feat: add func to resolve absolute file path

--- src/cli/io.c
+++ src/cli/io.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
+#include <limits.h>
 #include <sys/stat.h>
 
 #include "io.h"
 
-int get_file_stats(char *abs_path, FileStats *stats)
+int file_stats(char *path, FileStats *stats)
 {
+    char abs_path[PATH_MAX];
+    if (get_abs_path(path, abs_path) != 0)
+    {
+        return -1;
+    }
+
     stats->path = abs_path;
 
     struct stat file_info;
@@ -14,6 +21,16 @@ int get_file_stats(char *abs_path, FileStats *stats)
     }
 
     stats->size = file_info.st_size;
+
+    return 0;
+}
+
+int get_abs_path(char *rel_path, char *abs_path)
+{
+    if (realpath(rel_path, abs_path) == NULL)
+    {
+        return -1;
+    }
 
     return 0;
 }
--- src/cli/io.h
+++ src/cli/io.h
@@ -3,3 +3,7 @@ typedef struct
     const char *path;
     long long size;
 } FileStats;
+
+int file_stats(char *path, FileStats *stats);
+
+int get_abs_path(char *rel_path, char *abs_path);
